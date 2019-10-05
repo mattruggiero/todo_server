ec1e918ec38e8838403ab1ac5c1a6b2922769f09
Matt <matt@mattruggiero.com>
2019-10-04 22:39:48 -0700

    validation

---
+++ validator/validateRegister.js
@@ -0,0 +1,7 @@
+const validator = require('validator');
+const isEmpty = require('./isEmpty');
+
+const validateRegister = userRegistrationForm => {
+    let errors = {};
+    console.log("TODO: validateRegister");
+}
